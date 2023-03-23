34dba311e5d2a0edff33d49f05356f56dcb003e6
Paulo César <pcgms7@hotmail.com>
2023-03-22 21:51:18 -0300

    finalizando alterações com o redux toolkit

--- src/components/Produto/index.tsx
+++ src/components/Produto/index.tsx
@@ -1,11 +1,14 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { RootReducer } from '../../store'
+
 import { Produto as ProdutoType } from '../../App'
 import * as S from './styles'
 
+import { adicionar } from '../../store/reducers/carrrinho'
+import { favoritar } from '../../store/reducers/favoritar'
+
 type Props = {
   produto: ProdutoType
-  aoComprar: (produto: ProdutoType) => void
-  favoritar: (produto: ProdutoType) => void
-  estaNosFavoritos: boolean
 }
 
 export const paraReal = (valor: number) =>
@@ -13,12 +16,16 @@ export const paraReal = (valor: number) =>
     valor
   )
 
-const ProdutoComponent = ({
-  produto,
-  aoComprar,
-  favoritar,
-  estaNosFavoritos
-}: Props) => {
+const ProdutoComponent = ({ produto }: Props) => {
+  const dispatch = useDispatch()
+  const favoritos = useSelector((state: RootReducer) => state.favoritar.itens)
+
+  const estaNosFavoritos = favoritos.some((p) => p.id === produto.id)
+
+  const handleFavoritar = () => {
+    dispatch(favoritar(produto))
+  }
+
   return (
     <S.Produto>
       <S.Capa>
@@ -28,12 +35,12 @@ const ProdutoComponent = ({
       <S.Prices>
         <strong>{paraReal(produto.preco)}</strong>
       </S.Prices>
-      <S.BtnComprar onClick={() => favoritar(produto)} type="button">
+      <S.BtnComprar onClick={handleFavoritar} type="button">
         {estaNosFavoritos
           ? '- Remover dos favoritos'
           : '+ Adicionar aos favoritos'}
       </S.BtnComprar>
-      <S.BtnComprar onClick={() => aoComprar(produto)} type="button">
+      <S.BtnComprar onClick={() => dispatch(adicionar(produto))} type="button">
         Adicionar ao carrinho
       </S.BtnComprar>
     </S.Produto>
---
+++ src/store/index.ts
@@ -0,0 +1,19 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+import carrinhoReducer from './reducers/carrrinho'
+
+import favoritarReducer from './reducers/favoritar'
+
+import api from '../services/api'
+
+export const store = configureStore({
+  reducer: {
+    carrinho: carrinhoReducer,
+    favoritar: favoritarReducer,
+    [api.reducerPath]: api.reducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware)
+})
+
+export type RootReducer = ReturnType<typeof store.getState>
